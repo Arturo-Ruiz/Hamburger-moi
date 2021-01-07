769ae76923ded8968baf114f3970137d76412add
Arturo <gregory09ruiz@gmail.com>
2021-01-07 16:26:48 -0400

    test

--- app/Http/Controllers/Admin/AdminPanelController.php
+++ app/Http/Controllers/Admin/AdminPanelController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Auth;
+
+
 class AdminPanelController extends Controller
 {
     public function index(){
-        if(Auth::user()->role === '1'){
             return view('admin.index');
-        }else{
-            return redirect(route('login-admin'))->with('danger', 'Acceso Denegado');
-        }
     }
 }
